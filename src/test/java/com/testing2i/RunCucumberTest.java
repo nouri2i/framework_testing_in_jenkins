0f4ba5c12fbdbc2adceeda19a89196bc63252cfd
Abdennour Hachemi <abdennour.hachemi@2itesting.com>
2023-03-28 10:40:55 +0100

    updating pom.xml file

--- src/test/java/com/testing2i/RunCucumberTest.java
+++ src/test/java/com/testing2i/RunCucumberTest.java
@@ -1,5 +1,6 @@
 package com.testing2i;
 
+import org.junit.jupiter.api.Test;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
@@ -7,9 +8,9 @@ import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
-
+@Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("com/testing2i")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty" )
 public class RunCucumberTest {
 }
